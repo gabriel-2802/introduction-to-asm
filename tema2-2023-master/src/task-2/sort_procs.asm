64d44779d6ab66ec7edc136fbaa5a2fe4d574429
gabriel-2802 <gabrielvalentine738@gmail.com>
2023-05-08 22:53:17 +0300

    2

--- tema2-2023-master/src/task-2/sort_procs.asm
+++ tema2-2023-master/src/task-2/sort_procs.asm
@@ -11,7 +11,9 @@ section .text
 
 section .data
     length dd 0
-    offset dd 0
+    aux dd 0
+    ilen dd 0
+    jlen dd 0
 
 sort_procs:
     ;; DO NOT MODIFY
@@ -27,23 +29,99 @@ sort_procs:
     xor eax, eax
 
     ; aplicam selection sort
+    ; ecx -> i, ebx -> j
     xor ecx, ecx
 first_for:
     cmp ecx, [length]
     jge first_for_end
-    
+
     ; initializam contorul 2
+
+    ; calculam offset ul pt i
+    push ecx
+    imul ecx, proc_size
+    mov dword [ilen], ecx
+    pop ecx
+
 second_for:
     xor ebx, ebx
     mov ebx, ecx
-    cmp ecx, [length]
+    ; j = i + 1
+    inc ebx
+    cmp ebx, [length]
     jge second_for_end
 
-    
-    mov byte al, [edx + ecx + 2]
+    ; calculez offset petru j
+    push ebx
+    imul ebx, proc_size
+    mov dword [jlen], ebx
+    pop ebx
 
+    xor edi, edi
+    xor eax, eax
+    mov edi, dword [ilen]
+    mov al, [edx + edi + proc.prio]
+    mov edi, dword [jlen]
+    mov ah, [edx + edi + proc.prio]
+
+check_prio:
+    cmp al, ah
+    jl second_for_inc
+    jg swap
+    je check_time
+
+
+check_time:
+    ;am rams fara registri, deci va trebuie sa folosesc ebx
+    push ebx
+    xor eax, eax
+    xor ebx, ebx
+    xor edi, edi
+
+    mov edi, dword [ilen]
+    mov ax, [edx + edi + proc.time]
+
+    xor edi, edi
+    mov edi, dword [jlen]
+    mov bx, [edx + edi + proc.time]
+
+    cmp ax, bx
+    jl second_for_inc
+    je check_id
+    jg swap
+
+check_id:
+    xor eax, eax
+    xor ebx, ebx
+    xor edi, edi
+
+    mov edi, dword [ilen]
+    mov ax, [edx + edi + proc.pid]
+
+    xor edi, edi
+    mov edi, dword [jlen]
+    mov bx, [edx + edi + proc.pid]
+
+    cmp ax, bx
+    jle second_for_inc
+    jg swap
+
+swap:
+    ; structura depaseste 4 octetim deci va trebuie sa o mutam buc cu buc
+    xor eax, eax
+    xor ebx, ebx
+    xor edi, edi
+
+    mov edi, dword [ilen]
+    mov al, 
+
+second_for_inc:
+    ;incrementan j
+    pop ebx
     inc ebx
+
 second_for_end:
+    ;incrementan i
     inc ecx
 
 first_for_end:
